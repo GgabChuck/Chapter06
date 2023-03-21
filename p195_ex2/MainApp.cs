cddb2c015b010c01a0a3bfa6c53945f99fe0c050
Ggab_Chuck <rlqja3605@gmail.com>
2023-03-21 15:39:40 +0900

    2023.03.21 : 수업 완

--- p195_ex2/MainApp.cs
+++ p195_ex2/MainApp.cs
@@ -14,7 +14,15 @@ namespace p195_ex2
         }
         static void Main(string[] args)
         {
+            int x = 3;
+            int y = 4;
+            int z = 5;
 
+            Console.WriteLine($"x:{x}, y:{y}");
+
+            Swap(x, y, z);
+
+            Console.WriteLine($"x:{x}, y:{y}");
         }
     }
 }
