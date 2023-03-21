320bc850e7d2945a75505b0d5c9693a1bcdaed4d
Ggab_Chuck <rlqja3605@gmail.com>
2023-03-21 15:31:12 +0900

    2023.03.21 : Swap문

---
+++ p195_ex2/MainApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace p195_ex2
+{
+    class MainApp
+    {
+        public static void Swap(int a, int b, int c)
+        {
+            int temp = c;
+            int temp2 = b;
+            b = a;
+            c = temp2;
+            a = temp;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
